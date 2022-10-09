9eaca9aafac901cf8594ca5b14dc5ade04a76f8a
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-10-09 21:30:57 +0200

    Clean up unused and commented code

--- app/helpers/rewards_helper.rb
+++ app/helpers/rewards_helper.rb
@@ -27,7 +27,7 @@ module RewardsHelper
         Purchase.where(user_id: user.id, date:
           first_purchase_date..(first_purchase_date + 60.days)).order(date: :asc).all
       all_purchases_within_2_months.each do |purchase|
-        unless purchase.checked_for_free_movie_tickets
+        if !purchase.checked_for_free_movie_tickets
           @total_amount_for_movie_tickets += purchase.amount
           if @total_amount_for_movie_tickets >= 1000
             @free_movie_tickets = Reward.create(user_id: user.id, date: purchase.date, name: 'Free Movie Tickets').save
