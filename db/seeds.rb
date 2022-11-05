e93386e40a6bb502d317c663bac2a6d4117b3257
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-11-05 13:40:58 +0100

    Change the gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'faker'
+
 10.times do
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, birthday: Faker::Date.birthday(min_age: 18, max_age: 65), country_of_origin: Faker::Address.country, level: Faker::Number.within(range: 1..2), points: 0, tier: "Standard", birthday_reward: false, total_amount_spent: 0, total_amount_spent_foreign: 0, unused_amount: 0, unused_amount_foreign: 0)
 end
